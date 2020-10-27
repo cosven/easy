1887a6e33fe5068ec5cc8be8989f3aaebe880445
cosven <yinshaowen241@gmail.com>
2020-10-27 02:06:37 +0000

    add compaction_filter test cases demo

--- cases/compaction_filter_test.go
+++ cases/compaction_filter_test.go
@@ -1,7 +1,11 @@
 package cases
 
 import (
+	"bytes"
+	"context"
 	"encoding/base64"
+	"encoding/binary"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,11 +13,23 @@ import (
 	"testing"
 	"time"
 
+	"github.com/pingcap/errors"
 	"github.com/pingcap/parser/model"
 	"github.com/pingcap/tidb/tablecodec"
 	"github.com/pingcap/tidb/types"
 	"github.com/pingcap/tidb/util/codec"
+
+	"github.com/tikv/client-go/config"
+	"github.com/tikv/client-go/rawkv"
 )
+
+// x use modules
+func x() string {
+	if bytes.Equal([]byte{'x'}, []byte{'y'}) {
+		return hex.EncodeToString([]byte{'z'})
+	}
+	return "miao"
+}
 
 func decodeKey(key []byte) error {
 	b, decoded, err := codec.DecodeBytes(key, nil)
@@ -40,7 +56,61 @@ func encodeRowKey(tableId, rowId int64) []byte {
 	key = codec.EncodeInt(key, tableId)
 	key = append(key, []byte("_r")...)
 	key = codec.EncodeInt(key, rowId)
-	return key
+	return codec.EncodeBytes([]byte{}, key)
+}
+
+func KvEncodeKey(key []byte, ts uint64) []byte {
+	return codec.EncodeUintDesc(key, ts)
+}
+
+func EncodeValue(writeType []byte, startTs uint64, value []byte) {
+
+}
+
+func DecodeUint8(b []byte) ([]byte, uint8, error) {
+	if len(b) < 1 {
+		return nil, 0, errors.New("insufficient bytes to decode value")
+	}
+
+	var v uint8
+	buf := bytes.NewReader(b)
+	err := binary.Read(buf, binary.BigEndian, &v)
+	if err != nil {
+		return b, 0, err
+	}
+	b = b[1:]
+	return b, v, nil
+}
+
+func DecodeValue(b []byte) error {
+	b, writeType, err := DecodeUint8(b)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("write_type: %v\n", string(byte(writeType)))
+	b, startTs, err := codec.DecodeUvarint(b)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("start_ts: %v\n", startTs)
+	// TODO: currently, we assume the value is a shortvalue
+	// read the short value prefix b'v'
+	b, _, err = DecodeUint8(b)
+	if err != nil {
+		return err
+	}
+	b, length, err := DecodeUint8(b)
+	if err != nil {
+		return err
+	}
+	if len(b) < int(length) {
+		panic(fmt.Sprintf("content len %d shorter that value len %d", len(b), length))
+	}
+	value := b[:length]
+	// TODO: currently, we ignore the remain bytes
+	fmt.Printf("value: %s\n", string(value))
+	DecodeRow(value)
+	return nil
 }
 
 func DecodeRow(value []byte) error {
@@ -82,6 +152,26 @@ func TestDecodeRow(t *testing.T) {
 		panic(err)
 	}
 	if err := DecodeRow(data); err != nil {
+		panic(err)
+	}
+}
+
+func TestRawkv(t *testing.T) {
+	cli, err := rawkv.NewClient(context.TODO(), []string{"127.0.0.1:2379"}, config.Default())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	key := encodeRowKey(45, 2)
+	commitTs := uint64(420326424217649154)
+	kvKey := KvEncodeKey(key, commitTs)
+	value, err := cli.Get(context.TODO(), kvKey, rawkv.GetOption{Cf: rawkv.CfWrite})
+	if err != nil {
+		panic(err)
+	}
+	err = DecodeValue(value)
+	if err != nil {
 		panic(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,12 +1,16 @@
-module github.com/cosven/tidb-testing/easy
+module github.com/cosven/easy
 
 go 1.14
 
-require (
-	github.com/pingcap/log v0.0.0-20200828042413-fce0951f1463
-	go.etcd.io/etcd/v3 v3.0.0-00010101000000-000000000000
-	go.uber.org/zap v1.15.0
-)
+replace github.com/tikv/client-go v0.0.0-20201015080021-528475568618 => ../client-go
 
 replace go.etcd.io/etcd/v3 => github.com/etcd-io/etcd/v3 v3.3.0-rc.0.0.20200826232710-c20cc05fc548
-replace github.com/pingcap/kvproto v0.0.0-20200828054126-d677e6fd224a => github.com/gengliqi/kvproto v0.0.0-20200902152943-af12ea861cbf
+
+replace github.com/pingcap/kvproto v0.0.0-20201023092649-e6d6090277c9 => github.com/gengliqi/kvproto v0.0.0-20200902152943-af12ea861cbf
+
+require (
+	github.com/pingcap/errors v0.11.5-0.20201021055732-210aacd3fd99
+	github.com/pingcap/parser v0.0.0-20201024025010-3b2fb4b41d73
+	github.com/pingcap/tidb v1.1.0-beta.0.20201026110301-928c35de796e
+	github.com/tikv/client-go v0.0.0-20201015080021-528475568618
+)
